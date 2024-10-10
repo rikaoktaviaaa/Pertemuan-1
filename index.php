a1677e5083dba87842839708e3584fb83dc724f4
rikaoktaviaaa <rikaoktav13@gmail.com>
2024-10-10 14:54:27 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$nama = "Rika Oktavia";
+$umur = 19;
+?>
+
+
+<!DOCTYPE html>
+ <html>
+ <head>
+ <title>Contoh Variabel PHP</title>
+ </head>
+<body>
+    <h1>Halo, <?php echo $nama; ?>!</h1>
+ <p>Umur Anda adalah <?php echo $umur; ?> tahun.</p>
+</body>
+</html>
